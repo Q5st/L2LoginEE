54962cf939b423024314520ff25e41f08419f38c
Q5st <danny190100@gmail.com>
2022-06-29 20:28:02 +0300

    third

--- src/main/java/academy/prog/LoginServlet.java
+++ src/main/java/academy/prog/LoginServlet.java
@@ -17,18 +17,18 @@ public class LoginServlet extends HttpServlet {
         char [] passwordChar = password.toCharArray();
 
 
-        if (LOGIN.equals(login) && passwordChar.length>10 && ageInt>18) {
+        if (LOGIN.equals(login) && passwordChar.length>10 && ageInt>=18) {
             HttpSession session = request.getSession(true);
             session.setAttribute("user_login", login);
             response.sendRedirect("index.jsp");
         }else if (LOGIN.equals(login) && passwordChar.length>10 && ageInt<18) {
             PrintWriter pw = response.getWriter();
-            pw.println("<html><head><title>Prog.kiev.ua Test</title></head>");
+            pw.println("<html><head><title>Age </title></head>");
             pw.println("<body><h1>You are under eighteen</h1></body></html>");
             pw.println("<br>Click this link to <a href=\"/login?a=exit\".> return </a></body></html>");
         }else {
             PrintWriter pw = response.getWriter();
-            pw.println("<html><head><title>Prog.kiev.ua Test</title></head>");
+            pw.println("<html><head><title>Wrong </title></head>");
             pw.println("<body><h1>Wrong login or password</h1>");
             pw.println("<br>Click this link to <a href=\"/login?a=exit\"> return </a></body></html>");
         }
