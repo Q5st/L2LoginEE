675c32824741dcd3c9209482736c4a862e93e3c5
Q5st <danny190100@gmail.com>
2022-06-29 16:20:02 +0300

    second

---
+++ src/main/java/academy/prog/LoginServlet.java
@@ -0,0 +1,46 @@
+package academy.prog;
+
+import jakarta.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class LoginServlet extends HttpServlet {
+
+    static final String LOGIN = "admin";
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+        String ageStr = request.getParameter("age");
+        int ageInt = Integer.parseInt(ageStr);
+        char [] passwordChar = password.toCharArray();
+
+
+        if (LOGIN.equals(login) && passwordChar.length>10 && ageInt>18) {
+            HttpSession session = request.getSession(true);
+            session.setAttribute("user_login", login);
+            response.sendRedirect("index.jsp");
+        }else if (LOGIN.equals(login) && passwordChar.length>10 && ageInt<18) {
+            PrintWriter pw = response.getWriter();
+            pw.println("<html><head><title>Prog.kiev.ua Test</title></head>");
+            pw.println("<body><h1>You are under eighteen</h1></body></html>");
+            pw.println("<br>Click this link to <a href=\"/login?a=exit\".> return </a></body></html>");
+        }else {
+            PrintWriter pw = response.getWriter();
+            pw.println("<html><head><title>Prog.kiev.ua Test</title></head>");
+            pw.println("<body><h1>Wrong login or password</h1>");
+            pw.println("<br>Click this link to <a href=\"/login?a=exit\"> return </a></body></html>");
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String a = request.getParameter("a");
+        HttpSession session = request.getSession(false);
+
+        if ("exit".equals(a) && (session != null))
+            session.removeAttribute("user_login");
+
+        response.sendRedirect("index.jsp");
+    }
+}
