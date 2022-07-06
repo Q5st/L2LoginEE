b42b7ae2f40943d17f2868b9cb8a4e6eb403108d
Q5st <danny190100@gmail.com>
2022-07-06 09:46:57 +0300

    passwordCheck

--- src/main/java/academy/prog/LoginServlet.java
+++ src/main/java/academy/prog/LoginServlet.java
@@ -17,11 +17,11 @@ public class LoginServlet extends HttpServlet {
         char [] passwordChar = password.toCharArray();
 
 
-        if (LOGIN.equals(login) && passwordChar.length>10 && ageInt>=18) {
+        if (LOGIN.equals(login) && passwordCheck(password) && ageInt>=18) {
             HttpSession session = request.getSession(true);
             session.setAttribute("user_login", login);
             response.sendRedirect("index.jsp");
-        }else if (LOGIN.equals(login) && passwordChar.length>10 && ageInt<18) {
+        }else if (LOGIN.equals(login) && passwordCheck(password) && ageInt<18) {
             PrintWriter pw = response.getWriter();
             pw.println("<html><head><title>Age </title></head>");
             pw.println("<body><h1>You are under eighteen</h1></body></html>");
@@ -31,6 +31,14 @@ public class LoginServlet extends HttpServlet {
             pw.println("<html><head><title>Wrong </title></head>");
             pw.println("<body><h1>Wrong login or password</h1>");
             pw.println("<br>Click this link to <a href=\"/login?a=exit\"> return </a></body></html>");
+        }
+    }
+
+    private boolean passwordCheck(String psw){
+        if(psw.length()>10 && psw.matches(".*\\d+.*") && psw.matches(".*[A-Z]+.*") && psw.matches(".*[a-z]+.*") && psw.matches(".*[!@#$%^&?_]+.*")){
+            return true;
+        }else {
+            return false;
         }
     }
 
